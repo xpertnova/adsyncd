41b07fee0fbe44ca6063a354765cfa7146eec940
Lovis König <73945869+lvskng@users.noreply.github.com>
2022-12-16 13:06:20 +0100

    Add files via upload

---
+++ UserAdministration/__init__.py
@@ -0,0 +1,10 @@
+class UserAdministration:
+    _users = []
+    def __init__(self):
+        pass
+    def syncUsers(self):
+        pass
+    def getUsernameList(self):
+        pass
+    def userExists(self, username):
+        pass
