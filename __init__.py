e2203afb10eb1c5f6fc948cd826698924cd87976
Lovis König <lovis.koenig@xpertnova.de>
2022-12-16 12:51:24 +0100

    Worked on Daemonization. Caould it work now? Also added shebang for __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
 import daemon
+import lockfile
 import schedule
 import time
 
 
 from main import AzureSyncHandler
 
-with daemon.DaemonContext():
+with daemon.DaemonContext(uid=0, gid=0, working_directory="/var/adsyncd", pidfile=lockfile.FileLock("/var/run/adsyncd.pid")):
     handler = AzureSyncHandler()
     schedule.every(60).minutes.do(handler.syncUsers())
     while True:
