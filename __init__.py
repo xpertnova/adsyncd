880bc3b2182338d59e55db5912b5d463a3f88c95
Lovis König <lovis.koenig@xpertnova.de>
2022-12-16 13:32:44 +0100

    Attempted fix no lock file

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import time
 
 from main import AzureSyncHandler
 
-with daemon.DaemonContext(uid=0, gid=0, working_directory="/var/adsyncd", pidfile=lockfile.FileLock("/var/run/adsyncd.pid")):
+with daemon.DaemonContext(uid=0, gid=0, working_directory="/var/adsyncd", pidfile=daemon.pidfile.PIDLockFile("/var/run/adsyncd.pid")):
     handler = AzureSyncHandler()
     schedule.every(60).minutes.do(handler.syncUsers())
     while True:
