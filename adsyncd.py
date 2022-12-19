b6a33fd4cb84d38dc52997da4a047e432e9e4d92
Lovis König <lovis.koenig@xpertnova.de>
2022-12-19 11:15:39 +0100

    Refactor

--- adsyncd.py
+++ adsyncd.py
@@ -6,7 +6,11 @@ import time
 from lockfile.pidlockfile import PIDLockFile
 from AzureSyncHandler import AzureSyncHandler
 
+#Appending Python path to ./lib folder
+sys.path.append("/var/adsyncd/lib")
+#Creating PID lockfile
 pidfile = PIDLockFile("/var/run/adsyncd.pid")
+#Creating Daemon
 with daemon.DaemonContext(uid=0, gid=0, working_directory="/var/adsyncd", pidfile=pidfile) as context:
     handler = AzureSyncHandler()
     schedule.every(10).minutes.do(handler.syncUsers)  # Every 10 minutes check for new users
